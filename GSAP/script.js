33b7d2b6787e3e088d5bdf8bf5a7044bbdee30ca
nikhilkawa07 <144580516+nikhilkawa07@users.noreply.github.com>
2025-06-15 00:27:32 +0530

    Delete GSAP directory

--- GSAP/script.js
+++
@@ -1,5 +0,0 @@
-Shery.imageEffect(".image-div",{
-    style:6,
-    debug:true,
-    gooey:true
-  })
--- GSAP/script5.js
+++
@@ -1,6 +0,0 @@
-import LocomotiveScroll from 'locomotive-scroll';
-
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
